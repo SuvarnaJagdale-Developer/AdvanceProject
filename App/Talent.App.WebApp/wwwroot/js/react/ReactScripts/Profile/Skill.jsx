6aec785909fccdca3a42506dd1a6085e41a86e5e
suvarna <suvarnapjagdale@gmail.com>
2021-08-14 16:10:34 +1200

    Initial commit

---
+++ App/Talent.App.WebApp/wwwroot/js/react/ReactScripts/Profile/Skill.jsx
@@ -0,0 +1,329 @@
+﻿/* Skill section */
+import React ,{ Fragment }from 'react';
+import Cookies from 'js-cookie';
+import { ChildSingleInput } from '../Form/SingleInput.jsx';
+import { Table, Button, Icon, Grid,Segment,Menu,Dropdown} from 'semantic-ui-react';
+
+export default class Skill extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            editSkillId:"",
+            addSkill:{
+                id: "",
+                Skill:"",
+                ExperienceLevel:"",
+               
+               },
+                showEditSection: false,
+                addeOrUpdate: "",
+            }
+
+            this.openNew = this.openNew.bind(this)
+            this.closeEdit = this.closeEdit.bind(this)
+            this.saveData=this.saveData.bind(this)
+            this.updateStateData = this.updateStateData.bind(this)
+            this.handleChange=this.handleChange.bind(this)
+            this.openEdit = this.openEdit.bind(this)
+      
+    }
+
+    openNew() {
+       
+        console.log("new Open" +this.props.skillData),
+        this.setState({
+            showEditSection: true,
+            addSkill:{
+                id: "",
+                Skill:"",
+                ExperienceLevel:"",
+               
+               },
+         addeOrUpdate:"Create"
+        })
+       
+        
+    }
+
+
+    openEdit(skills) {
+     console.log("Skills Not Updated",skills.level)
+    
+      this.setState({
+        editSkillId:skills.id,
+          showEditSection: true,
+          addSkill:{
+            id: skills.id,
+            Skill:skills.name,
+            ExperienceLevel:skills.level
+           
+           },
+        addeOrUpdate:"Update"
+      })
+    }
+
+    updateStateData(event) {
+      const data = Object.assign({}, this.state.addSkill)
+      
+       data[event.target.name] = event.target.value
+       
+       this.setState({
+        addSkill:data
+         
+       })
+       console.log("set",data);
+   }
+
+   handleChange(event,data) 
+   {
+         
+     const{addSkill}=this.state
+     addSkill.ExperienceLevel = data.value
+     this.setState({
+      addSkill 
+  })
+       
+}
+   
+    closeEdit() {
+        console.log("close")
+              this.setState({
+                  showEditSection: false,
+                  editSkillId:false
+                   
+
+              })
+          }
+
+          saveData()
+          {
+              console.log("saveData",this.state.addSkill)
+          
+              var cookies = Cookies.get('talentAuthToken');
+              $.ajax({
+                url: 'http://localhost:60290/profile/profile/addSkill',
+                headers: {
+                    'Authorization': 'Bearer ' + cookies,
+                    'Content-Type': 'application/json'
+                },
+                type: "POST",
+                data: JSON.stringify(this.state.addSkill),
+                success: function (res) {
+      
+                console.log("Load Data",res)
+                if (res.success == true) {
+                  console.log("getDataSkill",res)
+                  if (this.state.createOrUpdate === "Create")
+                    {
+                        TalentUtil.notification.show("Skill added sucessfully", "success", null, null)
+                    }                    
+                    else
+                    {
+                        TalentUtil.notification.show("Skill updated sucessfully", "success", null, null)
+                    } 
+                  this.props.loadData()
+      
+                  } 
+              else {
+                  TalentUtil.notification.show("Skill not update successfully", "error", null, null)
+              }
+               }.bind(this),
+                error: function (res, a, b) {
+                  console.log(res)
+                  console.log(a)
+                  console.log(b)
+              }  
+              }); 
+             this.setState({showEditSection: false}) 
+             this.setState({editSkillId:""}) 
+              }
+
+
+              deleteLanguage(skillsId) 
+              {
+    
+                var cookies = Cookies.get('talentAuthToken');
+                $.ajax({
+                    url: 'http://localhost:60290/profile/profile/DeleteSkill',
+                    headers: {
+                        'Authorization': 'Bearer ' + cookies,
+                        'Content-Type': 'application/json'
+                    },
+                    type: "POST",
+                    data: JSON.stringify(skillsId),
+                    success: function (res) {
+                        console.log(res)
+                        if (res.success == true) 
+                        { 
+                          if (this.state.addeOrUpdate === "Create")
+                                {
+                                    TalentUtil.notification.show("Language added sucessfully", "success", null, null)
+                                }                    
+                                else
+                                {
+                                    TalentUtil.notification.show("Language updated sucessfully", "success", null, null)
+                                }                    
+                                this.props.loadData()
+                            } else {
+                                TalentUtil.notification.show("Language did not add successfully", "error", null, null)
+                     }
+                    }.bind(this),
+                    error: function (res, a, b) {
+                        console.log(res)
+                        console.log(a)
+                        console.log(b)
+                    }
+                })
+            }
+            
+            
+
+
+    render() {
+        return (
+            
+            this.state.showEditSection ? this.renderNew() : this.renderDisplay()
+        )
+    }
+    renderNew() {
+
+        const options = [
+            { key: 1, text: 'Beginner', value:'Beginner'},
+            { key: 2, text: 'Intermediate', value:'Intermediate'},
+            { key: 3, text: 'Expert', value:'Expert'},
+          ]
+             
+         return(
+             <div class='row'>
+               <div className='ui sixteen wide column'>
+                    <Grid columns='equal'>
+                   
+                      <Grid.Column>
+                      <ChildSingleInput
+                        inputType="text"
+                        name="Skill"
+                        value={this.state.addSkill.Skill}
+                        controlFunc={this.updateStateData}
+                        maxLength={80}
+                        placeholder="Add New Skill"
+                        errorMessage="Please enter a valid name"/>
+                        </Grid.Column>
+
+                        <Grid.Column> 
+                        <Dropdown
+                                placeholder='Select Level'
+                                fluid
+                                selection
+                                options={options}
+                                value={this.state.addSkill.ExperienceLevel} 
+                                onChange={(event,{ value  })=>this.handleChange (event,{ value })}  
+                                
+                            />
+
+                        </Grid.Column> 
+                        <Grid.Column> 
+                        <button type="button" className="ui teal button" onClick={this.saveData}>Add</button>
+                        <button type="button" className="ui button" onClick={this.closeEdit}>Cancel</button>
+                        </Grid.Column>
+                        </Grid>
+                      {this.renderDisplay()}
+                      </div>
+                      </div>
+                      )
+            }
+            
+   
+   renderDisplay()
+            {
+
+                const options = [
+                    { key: 1, text: 'Beginner', value:'Beginner'},
+                    { key: 2, text: 'Intermediate', value:'Intermediate'},
+                    { key: 3, text: 'Expert', value:'Expert'},
+                  ]
+             return (
+ 
+                <div className='ui sixteen wide column'>
+                 <h4> </h4>
+            <Table> 
+            <Table.Header> 
+            <Table.Row>
+           
+            <Table.HeaderCell>Skill</Table.HeaderCell>
+            <Table.HeaderCell>Level</Table.HeaderCell>
+            <Table.HeaderCell>
+            <button type="button" className="ui teal button" onClick={this.openNew}>
+            <Icon name='plus'/>
+              Add</button>
+            </Table.HeaderCell>
+              </Table.Row> 
+                </Table.Header>
+                
+            <Table.Body>
+                {this.props.skillData.map((skills) => (
+                     
+                     <Fragment>
+                     {this.state.editSkillId===skills.id ?(  
+                       
+                       <Table.Row>
+                       <Table.Cell>
+                       
+                       <ChildSingleInput
+                        inputType="text"
+                        name="Skill"
+                        value={this.state.addSkill.Skill}
+                        controlFunc={this.updateStateData}
+                        maxLength={80}
+                        placeholder="Add New Skill"
+                        errorMessage="Please enter a valid name"/>
+                          </Table.Cell>   
+
+                          <Table.Cell>
+                          <Dropdown
+                                placeholder='Select Level'
+                                fluid
+                                selection
+                                options={options}
+                                value={this.state.addSkill.ExperienceLevel} 
+                                onChange={(event,{ value  })=>this.handleChange (event,{ value })}  
+                                
+                            />
+                              </Table.Cell> 
+                                 
+                                 <Table.Cell>
+    
+                                 <button type="button" class="ui blue basic button" onClick={this.saveData}>Update</button>
+                               <button type="button"class="ui red basic button" onClick={this.closeEdit}>Cancel</button>
+                           
+                               </Table.Cell>
+                               </Table.Row>
+                              
+                              ) :(
+                       
+                       
+                       
+                       
+                       
+                       
+                       
+                       <Table.Row key={skills.id}>
+                       <Table.Cell>{skills.name}</Table.Cell>
+                       <Table.Cell>{skills.level}</Table.Cell>
+                      <Table.Cell> <Icon name='pencil alternate' onClick={()=>this.openEdit(skills)}/>
+                      <i className="delete icon" onClick={()=>this.deleteLanguage(skills.id)}></i></Table.Cell>
+                        
+                     </Table.Row>
+                      ) 
+                    }
+                    </Fragment>
+                  
+                 ))} 
+                 </Table.Body> 
+                </Table>
+                </div>
+          );
+
+        }
+ 
+}
+
---
+++ App/Talent.App.WebApp/wwwroot/js/react/ReactScripts/Profile/SocialMediaLinkedAccount.jsx
@@ -0,0 +1,152 @@
+﻿/* Social media JSX */
+import React from 'react';
+import { ChildSingleInput } from '../Form/SingleInput.jsx';
+import { Popup,Button,Icon} from 'semantic-ui-react';
+
+export default class SocialMediaLinkedAccount extends React.Component {
+    constructor(props) {
+        super(props);
+        const details = props.linkedAccounts ?
+        Object.assign({}, props.saveProfileData)
+        : {
+            linkedIn:"",
+            github: ""
+          }
+
+        this.state = {
+            showEditSection: false,
+            newContact: details
+        }
+        this.openEdit = this.openEdit.bind(this)
+        this.closeEdit = this.closeEdit.bind(this)
+        this.handleChange = this.handleChange.bind(this)
+        this.saveContact = this.saveContact.bind(this)
+        this.renderEdit = this.renderEdit.bind(this)
+        this.renderDisplay = this.renderDisplay.bind(this)
+    
+
+    }
+
+
+    openEdit() {
+        const details = Object.assign({}, this.props.linkedAccounts)
+        
+        this.setState({
+            showEditSection: true,
+            newContact: details
+        })
+        console.log("Details",this.state.newContact)
+    }
+
+    closeEdit() {
+        this.setState({
+            showEditSection: false
+        })
+    }
+    handleChange(event) {
+        const data = Object.assign({}, this.state.newContact)
+       
+
+        data[event.target.name] = event.target.value
+        this.setState({
+            newContact: data
+        })
+    }
+    saveContact() {
+       
+      
+        const data = Object.assign({}, this.state.newContact)
+      
+       
+        this.props.saveProfileData(({ linkedAccounts : { linkedIn : data.linkedIn,github:data.github}}));
+       
+        this.closeEdit()
+    }
+
+
+    componentDidMount() {
+        $('.ui.button.social-media')
+            .popup();
+    }
+
+
+    render() {
+        return (
+            
+           
+            this.state.showEditSection ? this.renderEdit() : this.renderDisplay()
+        )
+    }
+   
+    renderEdit() {
+        
+        //let linked = this.props.details ? this.props.details.linkedIn : ""
+        
+
+        return (
+           
+            <div className='ui sixteen wide column'>
+                <ChildSingleInput
+                    inputType="text"
+                    label="LinkedIn"
+                    name="linkedIn"
+                    value={this.state.newContact.linkedIn}
+                    controlFunc={this.handleChange}
+                    maxLength={80}
+                    placeholder="Enter your LinkedIn Url"
+                    errorMessage="Please enter a LinkedIn Url"
+                />
+                <ChildSingleInput
+                    inputType="text"
+                    label="GitHub"
+                    name="github"
+                    value={this.state.newContact.github}
+                    controlFunc={this.handleChange}
+                    maxLength={80}
+                    placeholder="Enter your Github Url"
+                    errorMessage="Please enter a valid GitHub Url"
+                />
+                 <button type="button" className="ui teal button" onClick={this.saveContact}>Save</button>
+                <button type="button" className="ui button" onClick={this.closeEdit}>Cancel</button>
+ 
+                </div>
+              
+
+)
+    }
+
+
+    renderDisplay() {
+        let linkedIn = this.props.linkedAccounts ? this.props.linkedAccounts.linkedIn : ""
+        let github = this.props.linkedAccounts ? this.props.linkedAccounts.github : ""
+        return (
+            <div className='ui sixteen wide column'>
+            <div>
+
+            <React.Fragment>
+                        <a href={linkedIn} className="ui linkedin button socialMedia">
+                            <i className="linkedin icon"></i>
+                            LinkedIn
+                        </a>
+                        <a href={github} className="ui floated teal button socialMedia">
+                            <i className="github icon"></i>
+                            GitHub
+                        </a>
+                    </React.Fragment>
+
+               {/*  <Button color='linkedin'>
+                <Icon name='linkedin'/> LinkedIn 
+                </Button>
+                <Button content='Github'secondary > 
+                <Icon name='github'/> Github
+                </Button> */}
+                <button type="button" className="ui right floated teal button" onClick={this.openEdit}>Edit</button>
+          </div>
+          </div>
+         
+        )
+       
+
+    }
+
+}
